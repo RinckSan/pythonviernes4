1ee745508c1fb6d954c3143f7287592a7d0eeb4a
209 <209@academicos.cesde.edu.co>
2022-08-26 20:52:19 -0500

    NTV4 Se crean ejemplos de bucles

---
+++ ciclofor.py
@@ -0,0 +1,13 @@
+#Ciclo for con python
+
+for i in range (1,10,2): #numero desde el que empieza, cantidad de veces que lo hara, de cuanto sera el intervalo
+    print(i)
+
+#Listas con Python
+
+nombres=["juan","sara","carlos","martha","silvia"]
+
+#Recorriendo una lista con python
+
+for nombre in nombres:
+    print(nombres)
